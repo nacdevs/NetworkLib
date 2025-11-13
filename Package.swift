5dc48a52da0c7e190278bb9347b6c13ec8dc9933
Nestor Camela <nestorcamela@gmail.com>
2025-11-13 12:07:45 -0300

    first commit

--- Package.swift
+++ Package.swift
@@ -1,21 +1,26 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyLibrary",
             targets: ["MyLibrary"]),
     ],
+    dependencies: [
+          .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
+            name: "MyLibrary",
+            dependencies: ["Alamofire"]),
         .testTarget(
             name: "MyLibraryTests",
             dependencies: ["MyLibrary"]
--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+import Alamofire
+
+
+public class TestLib{
+
+    @available(iOS 13.0.0, *)
+    public func getRequest<T: Decodable>(url: String, type: T.Type) async throws -> T {
+           let dataTask = AF.request(url)
+               .validate()
+               .serializingDecodable(T.self)
+           
+           return try await dataTask.value
+    }
+}
