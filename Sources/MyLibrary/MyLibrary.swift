2a64f75e00c9c9241eb17d99d415749878aba753
Nestor Camela <nestorcamela@gmail.com>
2025-11-13 12:11:14 -0300

    fix cmommit

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -5,9 +5,10 @@ import Alamofire
 
 
 public class TestLib{
-
+    let url = "https://uselessfacts.jsph.pl/random.json"
+    
     @available(iOS 13.0.0, *)
-    public func getRequest<T: Decodable>(url: String, type: T.Type) async throws -> T {
+    public func getRequest<T: Decodable>(type: T.Type) async throws -> T {
            let dataTask = AF.request(url)
                .validate()
                .serializingDecodable(T.self)
